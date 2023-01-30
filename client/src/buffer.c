e6ddbad97e9e9f60f39dcd229ccb1225a9fe51c9
vishwanath1306 <vishwa.nath@outlook.com>
2023-01-30 13:59:27 -0600

    added buffer to file

--- client/src/buffer.c
+++ client/src/buffer.c
@@ -248,7 +248,8 @@ void buffer_write(Buffer* b, size_t size, char** dst, size_t* dst_size) {
 bool buffer_try_write_all(Buffer *b, char* buf, size_t buf_size) {
     if (b->remaining < buf_size+sizeof(size_t)) return false;
     printf("Buffer is: %s\n", buf);
-    printf("Size of buffer: %zu", buf_size);
+    printf("Size of buffer: %d\n", buf_size);
+    printf("Size of size(t): %d\n", sizeof(size_t));
     *(size_t *) b->ptr = buf_size; // might have to use htonl() and ntohl() if non-x86 boxes involved
     b->ptr += sizeof(size_t);
     memcpy((void*) b->ptr, (void*) buf, buf_size);
